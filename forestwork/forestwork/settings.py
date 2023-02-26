4487d4a0bf6b748a06f4b95d7b86b099a16d9823
Elvee Bolt <boltelvee@gmail.com>
2023-02-26 11:04:37 +0200

    Added DATETIME_FORMAT in settings

--- forestwork/forestwork/settings.py
+++ forestwork/forestwork/settings.py
@@ -117,6 +117,10 @@ USE_I18N = True
 
 USE_TZ = True
 
+DATETIME_FORMAT = 'd.m.Y H:i'
+
+USE_L10N = False
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
