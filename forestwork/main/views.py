d94b28e7a22a0a22f8e086b3bd90dcbd99050174
Elvee Bolt <boltelvee@gmail.com>
2023-02-27 16:39:40 +0200

    Added functionality for Index page

--- forestwork/main/views.py
+++ forestwork/main/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from jobs.models import Job
+from users.models import User
 
 
 # Create your views here.
-def index(request):
-    # TODO: add functionality Home page.
+class IndexView(TemplateView):
+    template_name = 'main/index.html'
+    extra_context = {
+        'title': 'ForestWork',
+        'subtitle': 'Сервис поиска работы и кандидатов'
+    }
 
-    context = {}
-    return render(request, 'main/index.html', context=context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['job_list'] = Job.objects.all()[:5]
+        context['employer_list'] = User.objects.filter(type=1)[:5]
+        context['developer_list'] = User.objects.filter(type=0)[:5]
+        return context
