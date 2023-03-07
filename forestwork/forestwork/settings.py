873ed42f01e21c7a965140b1d994cea4aaae5070
Elvee Bolt <boltelvee@gmail.com>
2023-03-07 16:13:54 +0200

    Refactoring env, docker compose and settings

--- forestwork/forestwork/settings.py
+++ forestwork/forestwork/settings.py
@@ -79,14 +79,6 @@ WSGI_APPLICATION = 'forestwork.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
         'ENGINE': os.environ.get('SQL_ENGINE', 'django.db.backends.sqlite3'),
